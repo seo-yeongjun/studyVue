0b2deec7199d6f3124934f3cf247aea9690f31c7
seoyeongjun <zanygeek8371@gmail.com>
2025-03-18 23:25:09 +0900

    2025.03.18

--- helloWorld/main.js
+++ helloWorld/main.js
@@ -1,7 +1,24 @@
+const functionOneToHundred = () => {
+    let arr = [];
+    for (let i = 1; i <= 100; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
+
 const app = new Vue({
     el: "#firstApp",
     data: {
         message: "Hello World!",
         image: "./assets/logo.png",
+        productNumber : 10,
+        oneTohundred: functionOneToHundred(),
+        number : 0,
     },
+    methods: {
+        resetNumber : function(){
+            this.number = 0;
+            console.log(this.number)
+        }
+    }
 })
